bae94673c8af9f1a9c64277618a3d9f2608ca19f
Kos Krzysztof <krzysztof.kos33@gmail.com>
2021-05-23 15:10:29 +0200

    Add new variable for script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+var computerMove, playerMove;
+playerMove = 'papier';
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
 function printMessage(msg){
 	var div = document.createElement('div');
 	div.innerHTML = msg;
